c12ef588bc930edfb8417bce300bcbddb3d3bb04
Kevin Saliou <kevin@saliou.name>
2013-12-12 12:04:20 +0100

    add example file + readme

--- example.php
+++ example.php
@@ -14,40 +14,40 @@ try {
     $xml = $client->api('baseball')->get_weather(22688, 'match');
 
     $xml = $client->api('basketball')->get_injuries(191, 'team');
-    $xml = $client->api('basketball')->get_player_statistics(408, 'season', array('team_id' => 178));
+    $xml = $client->api('basketball')->get_player_statistics(408, 'season', ['team_id' => 178]);
     $xml = $client->api('basketball')->get_rankings('ap_top_25');
 
-    $xml = $client->api('cricket')->get_seasons(array('authorized' => 'yes', 'active' => 'yes'));
-    $xml = $client->api('cricket')->get_squads(113, 'season', array('detailed' => 'yes'));
+    $xml = $client->api('cricket')->get_seasons(['authorized' => 'yes', 'active' => 'yes']);
+    $xml = $client->api('cricket')->get_squads(113, 'season', ['detailed' => 'yes']);
 
     $xml = $client->api('golf')->get_holebyhole(1294, 'round');
     $xml = $client->api('golf')->get_leaderboard(333, 'season');
-    $xml = $client->api('golf')->get_people(138, 'person', array('detailed' => 'yes'));
+    $xml = $client->api('golf')->get_people(138, 'person', ['detailed' => 'yes']);
 
-    $xml = $client->api('handball')->get_tables(114, 'season', array('tabletype' => 'form-home'));
+    $xml = $client->api('handball')->get_tables(114, 'season', ['tabletype' => 'form-home']);
 
     $xml = $client->api('hockey')->get_match_extra(102880);
 
-    $xml = $client->api('motorsports')->get_circuit(1, 'championship', array('detailed' => 'yes'));
-    $xml = $client->api('motorsports')->get_sessions(3013, 'session', array('detailed' => 'yes'));
-    $xml = $client->api('motorsports')->get_teammembers(3007, 'session', array('detailed' => 'yes'));
+    $xml = $client->api('motorsports')->get_circuit(1, 'championship', ['detailed' => 'yes']);
+    $xml = $client->api('motorsports')->get_sessions(3013, 'session', ['detailed' => 'yes']);
+    $xml = $client->api('motorsports')->get_teammembers(3007, 'session', ['detailed' => 'yes']);
 
-    $xml = $client->api('tennis')->get_doubles(31, 'double', array('detailed' => 'yes'));
-    $xml = $client->api('tennis')->get_players(31, 'double', array('gender' => 'male'));
+    $xml = $client->api('tennis')->get_doubles(31, 'double', ['detailed' => 'yes']);
+    $xml = $client->api('tennis')->get_players(31, 'double', ['gender' => 'male']);
     $xml = $client->api('tennis')->get_season_competitor(1741);
-    $xml = $client->api('tennis')->get_tours(array('type' => 'tour'));
+    $xml = $client->api('tennis')->get_tours(['type' => 'tour']);
 
     $xml = $client->api('soccer')->get_betting_statistics(8055, 'season', 'result');
     $xml = $client->api('soccer')->get_career(961, 'team');
     $xml = $client->api('soccer')->get_competitions();
-    $xml = $client->api('soccer')->get_deleted(array('type' => 'person'));
+    $xml = $client->api('soccer')->get_deleted(['type' => 'person']);
     $xml = $client->api('soccer')->get_hashtags(964, 'team');
-    $xml = $client->api('soccer')->get_match_commentary(1286864, array('source' => 'auto'));
-    $xml = $client->api('soccer')->get_match_editorials(1286861, array('text' => 'both'));
+    $xml = $client->api('soccer')->get_match_commentary(1286864, ['source' => 'auto']);
+    $xml = $client->api('soccer')->get_match_editorials(1286861, ['text' => 'both']);
     $xml = $client->api('soccer')->get_match_formations(1286869);
     $xml = $client->api('soccer')->get_matches(8318, 'season');
-    $xml = $client->api('soccer')->get_matches_live(array('now_playing' => 'no', 'minutes' => 'yes', 'type' => 'match', 'id' => 1445860, 'date' => '2013-06-11'));
-    $xml = $client->api('soccer')->get_matches_live_updates(array('last_updated' => '2012-03-23 13:25:00'));
+    $xml = $client->api('soccer')->get_matches_live(['now_playing' => 'no', 'minutes' => 'yes', 'type' => 'match', 'id' => 1445860, 'date' => '2013-06-11']);
+    $xml = $client->api('soccer')->get_matches_live_updates(['last_updated' => '2012-03-23 13:25:00']);
     $xml = $client->api('soccer')->get_players_abroad(80, 'area', '2012-11-15');
     $xml = $client->api('soccer')->get_referees('round', 13557);
     $xml = $client->api('soccer')->get_runningball_matches();
@@ -58,18 +58,18 @@ try {
     $xml = $client->api('soccer')->get_suspensions(1483412);
     $xml = $client->api('soccer')->get_suspensions_warning(1063217, 'match');
     $xml = $client->api('soccer')->get_tables(8055, 'season');
-    $xml = $client->api('soccer')->get_tables_cumulative(7299, 'season', array('lang' => 'en'));
+    $xml = $client->api('soccer')->get_tables_cumulative(7299, 'season', ['lang' => 'en']);
     $xml = $client->api('soccer')->get_tables_live(4943, 'season');
-    $xml = $client->api('soccer')->get_team_statistics(6951, array('season_id' => 8055, 'form' => 'no'));
-    $xml = $client->api('soccer')->get_teams(661, 'team', array('detailed' => 'yes'));
-    $xml = $client->api('soccer')->get_transfers(8, 'competition', array('proceeded' => 'yes'));
+    $xml = $client->api('soccer')->get_team_statistics(6951, ['season_id' => 8055, 'form' => 'no']);
+    $xml = $client->api('soccer')->get_teams(661, 'team', ['detailed' => 'yes']);
+    $xml = $client->api('soccer')->get_transfers(8, 'competition', ['proceeded' => 'yes']);
 
     print_r($xml);die;
 
     $xml = $client->api('soccer')->get_competitions();
-    $ret = array();
+    $ret = [];
     foreach ($xml->competition as $competition) {
-        $ret[(int) $competition['competition_id']] = array(
+        $ret[(int) $competition['competition_id']] = [
             'competition_id' => (int) $competition['competition_id'],
             'name' => (string) $competition['name'],
             'soccertype' => (string) $competition['soccertype'],
@@ -80,29 +80,29 @@ try {
             'area_name' => (string) $competition['area_name'],
             'last_updated' => (string) $competition['last_updated'],
             'countrycode' => (string) $competition['countrycode'],
-        );
+        ];
     }
     print_r($ret);die;
 
-    $xml = $client->api('soccer')->get_areas(array('area_id' => 7));
-    $ret = array();
+    $xml = $client->api('soccer')->get_areas(['area_id' => 7]);
+    $ret = [];
     foreach ($xml->area->area as $area) {
-        $ret[(int) $area['area_id']] = array(
+        $ret[(int) $area['area_id']] = [
             'area_id' => (int) $area['area_id'],
             'countrycode' => (string) $area['countrycode'],
             'name' => (string) $area['name'],
-        );
+        ];
     }
     print_r($ret);die;
 
-    $xml = $client->api('am_football')->get_areas(array('area_id' => 7));
-    $ret = array();
+    $xml = $client->api('am_football')->get_areas(['area_id' => 7]);
+    $ret = [];
     foreach ($xml->area->area as $area) {
-        $ret[(int) $area['area_id']] = array(
+        $ret[(int) $area['area_id']] = [
             'area_id' => (int) $area['area_id'],
             'countrycode' => (string) $area['countrycode'],
             'name' => (string) $area['name'],
-        );
+        ];
     }
     print_r($ret);die;
 
